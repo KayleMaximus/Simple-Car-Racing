7c0a5f2806070f1e335ea3daac6215aa502b4f47
Kayle Maximus <kaylemaximus@gmail.com>
2023-08-08 06:49:10 +0700

    add score system

---
+++ Simple-Car-Racing/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private float _scoreMultiplier;
+
+    private float _score;
+
+    void Update()
+    {
+        _score += Time.deltaTime * _scoreMultiplier;
+
+        _scoreText.text = Mathf.FloorToInt(_score).ToString();
+    }
+}
